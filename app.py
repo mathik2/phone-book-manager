f9a9098786764ecaba4e7086d073abaebee4fcfb
Mathivathana S <45286002+mathik2@users.noreply.github.com>
2025-10-02 12:37:14 +0530

    prepared for deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from processor import get_all_contacts, delete_contact, update_contact, search_contacts, add_contact
+import os
 
 app = Flask(__name__)
 
@@ -36,4 +37,5 @@ def update(name):
     return jsonify({'success': True})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
